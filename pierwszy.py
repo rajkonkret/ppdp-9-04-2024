28c1b8d70aeae107ca8ea7629f953e0ddb0a4492
rjaniak <rajkonkret660@gmail.com>
2024-04-09 11:51:57 +0200

    [1.2] - add pierwszy.py - typ tekstowy

---
+++ pierwszy.py
@@ -0,0 +1,17 @@
+print()  # wypisz/wydrukuj komunikat
+print("Hello, World!")  # Hello, World!
+print('Hello, World!')  # Hello, World!
+print('Hello, World!')  # Hello, World!
+print('Hello, World!')  # Hello, World!
+print('Hello, World!')  # Hello, World!
+print('Hello, World!')  # Hello, World!
+print('Hello, World!')  # Hello, World!
+# ctrl d - powielanie linijek
+print('Nazywam się Radek')
+# print("Nazywam się Radek')
+#   File "C:\Users\CSComarch\PycharmProjects\ppdp-9-04-2024\pierwszy.py", line 11
+#     print("Nazywam się Radek')
+#           ^
+# SyntaxError: unterminated string literal (detected at line 11)
+print(type("Nazywam się Radek"))  # type() - sprawdzenie typu danych  - <class 'str'> typ tekstowy
+
